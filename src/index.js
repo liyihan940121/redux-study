f4020449bbe313afb8f752eb5147b13e6b96f989
liyihan <liyihan@mininglamp.com>
2021-11-14 21:54:08 +0800

    update

--- src/index.js
+++ src/index.js
@@ -15,33 +15,50 @@ function Reducer (state = 0, action) {
   if (type === 'INCREMENT') {
     return state + 1 
   } else if (type === 'DECREMENT') {
-    return satate - 1
+    return state - 1
   } else {
-    return state
+    return state 
   }
 }
 // 2.基于 reducer 创建 Store
+const store = createStore(Reducer, 100)
 // 3.获取 store 的状态
+console.log(store.getState()) //获取最新的状态
 // 4.更新 store 的状态
+//  store.dispath({type: 行为类型，额外参数})
+setTimeout(function () {
+  // 发起dispath就是在调用 reducer
+  // dispatch 接收的参数叫做 action
+  store.dispatch({
+    type:'INCREMENT'
+  })
+}, 1000)
 // 5.监测 store 中 state 的变化，驱动视图更新
-
+store.subscribe(() => {
+  console.log(store.getState())
+  render()
+})
 
 const Counter = (props) => {
   return (
     <div>
-      <h1>{props.value}</h1>
+      <h1>{props.value}</h1> 
       <button>Increment</button>
       <button>Decrement</button>
     </div>
   )
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter value={100}/>
-  </React.StrictMode>,
+render()
+
+function render() {
+  ReactDOM.render(
+    <Counter value={ store.getState() }/>,
   document.getElementById('root')
 );
+
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
